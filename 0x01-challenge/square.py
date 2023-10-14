b28859fc8ecce43e7cce3afd3220371f9f5cba20
alidrisy <a.alidrisy6@egamil.com>
2023-10-15 00:54:17 +0300

    1. My square

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
+"""define a square class"""
+
 
 class square():
-
+    """ a square calss"""
     width = 0
     height = 0
 
     def __init__(self, *args, **kwargs):
+        """ initilaize data"""
         for key, value in kwargs.items():
             setattr(self, key, value)
 
@@ -14,9 +17,11 @@ class square():
         return self.width * self.height
 
     def PermiterOfMySquare(self):
+        """ return parametars of the square"""
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
+        """ print the param of the square"""
         return "{}/{}".format(self.width, self.height)
 
 
