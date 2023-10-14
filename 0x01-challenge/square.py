b7bc569727cfeedc1be15094be5ed6972331b85f
alidrisy <a.alidrisy6@egamil.com>
2023-10-15 00:57:08 +0300

    1. My square

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -28,7 +28,7 @@ class square():
 if __name__ == "__main__":
     """ Create a square object """
 
-    s = square(width=12, height=12)
+    s = square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
     print(s.PermiterOfMySquare())
