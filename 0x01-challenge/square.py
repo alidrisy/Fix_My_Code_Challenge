5eee96cabc77b734fa9e92b0d945eee8ce089711
alidrisy <a.alidrisy6@egamil.com>
2023-10-15 00:48:40 +0300

    1. My square

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -11,7 +11,7 @@ class square():
 
     def area_of_my_square(self):
         """ Area of the square """
-        return self.width * self.width
+        return self.width * self.height
 
     def PermiterOfMySquare(self):
         return (self.width * 2) + (self.height * 2)
