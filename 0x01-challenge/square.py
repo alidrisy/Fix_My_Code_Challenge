a3a4c534fcab25a979828d9d4c664e795e24d363
alidrisy <a.alidrisy6@gmail.com>
2023-10-16 00:29:51 +0300

    fix squar

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-"""Define a model for a Square class"""
+""" Model for a Square class """
 
 
 class square():
-    """A Square class"""
+    """ A Square class """
     width = 0
     height = 0
 
     def __init__(self, *args, **kwargs):
-        """initilaize data for the squar"""
+        """ Initilaize data for the square """
         for key, value in kwargs.items():
             setattr(self, key, value)
 
@@ -17,16 +17,15 @@ class square():
         return self.width * self.height
 
     def permiter_of_my_square(self):
-        """ return parametars of the square"""
+        """ Return parametars of the square """
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
-        """ print the param of the square"""
+        """ Print the param of the square """
         return "{}/{}".format(self.width, self.height)
 
 
 if __name__ == "__main__":
-    """ Create a square object """
     s = square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
